0f6abc8fe92235d30b0e5763f432b1d220c46f35
Andon Goshev <andon.go6ev@gmail.com>
2024-05-01 16:14:44 +0300

    A lot of things created. The skeleton is still under construction

---
+++ scripts/first-button.js
@@ -0,0 +1,15 @@
+function firstButton(){
+    // //Take button element
+    // const firstButton = document.getElementById("first-button")
+
+
+    // //Take h1 element
+    // h1Element = document.getElementById("h1")
+
+    // firstButton.addEventListener("click", () => {
+    //     firstButton.style.color = "red"
+    //     console.log("okay");
+    // })
+
+
+}
