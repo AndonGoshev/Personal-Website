dffc6d789e560f116a43a91aa6f0ca18d56afcdc
Andon Goshev <andon.go6ev@gmail.com>
2024-05-03 16:25:54 +0300

    lots of things changed

--- scripts/first-button.js
+++ scripts/first-button.js
@@ -1,15 +1,30 @@
 function firstButton(){
-    // //Take button element
-    // const firstButton = document.getElementById("first-button")
+    //Take button element
+    const firstButton = document.getElementById("first-button")
 
+    // Take mainsection element 
+    const mainSectionElement = document.querySelector("main")
 
-    // //Take h1 element
-    // h1Element = document.getElementById("h1")
+    //Take h1 element
+    h1Element = document.getElementById("h1")
 
-    // firstButton.addEventListener("click", () => {
-    //     firstButton.style.color = "red"
-    //     console.log("okay");
-    // })
+    firstButton.addEventListener("click", () => {
+        const firstStepElement = document.getElementById("first-step")
+        
+        if (firstStepElement.style.display == "block") {
+            firstStepElement.style.display = "none"
+            // mainSectionElement.scrollIntoView({behavior: "smooth", duration: 5000})
+        } else {
+            firstStepElement.style.display = "block"
+            // firstStepElement.scrollIntoView({behavior: "smooth", duration: 5000})
+            scrollTo(firstStepElement, 5000)
+        }
+
+        firstStepElement.classList.add("first-step")
+        
+        
+        console.log("okay");
+    })
 
 
 }
